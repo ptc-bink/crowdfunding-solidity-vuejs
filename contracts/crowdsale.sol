b7a7771bf9a3a7b9dbdf7dc48671fc756790d80f
sakele1026 <sakelejames@gmail.com>
2021-01-01 07:34:32 -0800

    working on a best frontend

---
+++ contracts/crowdsale.sol
@@ -0,0 +1,45 @@
+pragma solidity ^0.6.6;
+
+import "@openzeppelin/contracts/math/SafeMath.sol";
+
+contract Crowdfunding {
+    using SafeMath for uint256;
+
+    Project[] private projects;
+
+
+    event ProjectStarted(
+        address contractAddress,
+        address projectStarter,
+        string projectTitle,
+        string projectDesc,
+        uint256 deadline,
+        uint256 goalAmount
+    );
+
+    
+    function startProject(
+        string calldata title,
+        string calldata description,
+        uint durationInDays,
+        uint amountToRaise
+    ) external {
+        uint raiseUntil = now.add(durationInDays.mul(1 days));
+        Project newProject = new Project(msg.sender, title, description, raiseUntil, amountToRaise);
+        project.push(newProject);
+        emit ProjectStarted(
+            address(newProject),
+            msg.sender,
+            title,
+            description,
+            raiseUntil,
+            amountToRaise
+        );
+    }
+
+    function returnAllProjects() external view returns(Project[] memory){
+        return projects;
+    }
+}
+
+
